baa1c894e600143991e0806b98915ec2446a1844
Dmytro Popovskyi <popovskiy96@gmail.com>
2025-07-08 23:54:44 +0200

    done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './components/App/App.jsx'
+import '../node_modules/modern-normalize/modern-normalize.css'
+import '../node_modules/reset-css/reset.css'
 import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
